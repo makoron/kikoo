80117e10959441eabdd61611787de7b053f3e8c4
Makoto Kawai <igadon@me.com>
2021-11-07 20:33:52 +0900

    202111072033

--- tweet/views.py
+++ tweet/views.py
@@ -1,11 +1,17 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
 from .models import Post
 
-# Create your views here.
 
 class PostListView(ListView):
     model = Post
     template_name = "tweet/index.html"
-    context_object_name = "posts"
+    context_object_name = 'posts'
     ordering = ['-date_posted']
-    paginate_by = 5
+    paginate_= 5
+
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['content']
+    template_name = 'tweet/post_create.html'
+    success_url = '/'
--- user/urls.py
+++ user/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = 'user'
 
 urlpatterns = [
-    path('home/', views.HomeView.as_view(), name="home")
+    path('home/', views.HomeView.as_view(), name='home')
 ]
